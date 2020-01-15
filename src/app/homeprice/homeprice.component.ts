ca5eee44f041f31a6e4a1262526afa1a80036d0f
संभव <59118230+teamSupprt@users.noreply.github.com>
2020-01-16 00:42:40 +0530

    Add files via upload

---
+++ src/app/homeprice/homeprice.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-homeprice',
+  templateUrl: './homeprice.component.html',
+  styleUrls: ['./homeprice.component.css']
+})
+export class HomepriceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
