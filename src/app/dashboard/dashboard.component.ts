9f12797f5f610499da5d46bdd7ecac020a8281af
Ishaq Tasleem Mohammad <58343363+ismohammad@users.noreply.github.com>
2020-04-06 01:44:26 -0700

    New Service implementation started

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -6,6 +6,8 @@ import { config } from '../appconstants/config';
 import { ActivatedRoute } from '@angular/router';
 import { Ng4LoadingSpinnerService } from 'ng4-loading-spinner';
 import { AlertsService } from '../AppRestCall/alerts/alerts.service';
+import { TranslateService } from '@ngx-translate/core';
+
 
 @Component({
   selector: 'app-dashboard',
@@ -23,37 +25,57 @@ export class DashboardComponent implements OnInit {
     private route: ActivatedRoute,
     private spinnerService: Ng4LoadingSpinnerService,
     private alertService: AlertsService,
+    public translate: TranslateService
   ) {
     route.params.subscribe(params => {
-    this.name = params.name; });
-    }
+      this.name = params.name;
+    });
+    translate.addLangs([config.lang_english_word.toString(), config.lang_telugu_word.toString(), config.lang_hindi_word.toString()]);
+    translate.setDefaultLang(config.lang_english_word.toString());
+    const browserLang = translate.getBrowserLang();
+    translate.use(browserLang.match(/English|తెలుగు|हिंदी/) ? browserLang : config.lang_english_word.toString());
+
+  }
 
   ngOnInit() {
     if (this.name != null) {
       this.spinnerService.show();
       this.userService.getUserByUserId(this.userService.currentUserValue.userId).subscribe(
-      (userresp: any) => {
-        this.userService.setCurrentUserValue(userresp);
-        this.spinnerService.hide();
-      },
-      error => {
-        this.alertService.error(error);
-        this.spinnerService.hide();
-      });
+        (userresp: any) => {
+          this.userService.setCurrentUserValue(userresp);
+          this.spinnerService.hide();
+        },
+        error => {
+          this.alertService.error(error);
+          this.spinnerService.hide();
+        });
     }
 
     if (this.userService.currentUserValue.userroles.rolecode === config.user_rolecode_fu) {
       if (this.userService.currentUserValue.freelancehistoryentity[0].bgstatus ===
         config.bg_code_approved) {
-      this.showmenufu = true;
+        this.showmenufu = true;
       }
       if (this.userService.currentUserValue.freelancehistoryentity[0].bgstatus ===
         config.bg_code_rejected) {
         this.showmenufu = false;
       }
+    }
+    this.translateToLanguage(this.userService.currentUserValue.preferlang);
   }
-}
 
+  translateToLanguage(preferedLang: string) {
+    if (preferedLang === config.lang_code_hi.toString()) {
+      preferedLang = config.lang_hindi_word.toString();
+    }
+    if (preferedLang === config.lang_code_te.toString()) {
+      preferedLang = config.lang_telugu_word.toString();
+    }
+    if (preferedLang === config.default_prefer_lang.toString()) {
+      preferedLang = config.lang_english_word.toString();
+    }
+    this.translate.use(preferedLang);
+  }
   logout() {
     this.userService.logout();
     this.router.navigate(['/app']);
