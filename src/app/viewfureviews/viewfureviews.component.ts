efaf77c46f41e9f4ba5f9260413b039e4b0f099e
W4U <59118230+teamSupprt@users.noreply.github.com>
2020-11-10 00:42:24 -0800

    New signup of admin is done

---
+++ src/app/viewfureviews/viewfureviews.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-viewfureviews',
+  templateUrl: './viewfureviews.component.html',
+  styleUrls: ['./viewfureviews.component.scss']
+})
+export class ViewfureviewsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
